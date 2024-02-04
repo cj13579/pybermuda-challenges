d4e8afba88f264d6bde0f478fb4a4ef08e78e5f8
Chris Blake <cjb.blake@gmail.com>
2024-02-04 16:14:10 -0400

    :bug: lol, no w

--- 001/solver.py
+++ 001/solver.py
@@ -2,7 +2,7 @@ from wordle import Wordle
 import time
 import concurrent.futures
 
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 wordle = Wordle()
 letters = []
 
